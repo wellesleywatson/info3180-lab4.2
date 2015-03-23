fa8653c07a6397ebd5e58a0a009209a178b68fad
wellesleywatson <git@github.com:wellesleywatson>
2015-03-23 21:39:58 +0000

    game almost complete

---
+++ js/enemy_manager.js
@@ -0,0 +1,99 @@
+game.EnemyManager = me.Container.extend({
+  
+  init: function ()
+  {
+    this.COLS = 9;
+    this.ROWS = 4;
+    this.vel = 16;
+    this._super(me.Container, "init", [0, 32, this.COLS * 64 - 32, this.ROWS * 64 - 32]);
+  },
+  
+  createEnemies: function () 
+  {
+      for (var i = 0; i < this.COLS; i++) 
+      {
+          for (var j = 0; j < this.ROWS; j++)
+          {
+              this.addChild(me.pool.pull("enemy", i * 64, j * 64));
+          }
+      }
+      this.createdEnemies = true;
+   },
+  
+ onActivateEvent: function () 
+  {
+    var _this = this;
+    this.timer = me.timer.setInterval(function () 
+     {
+        var bounds = _this.childBounds;
+        var right = _this.pos.x + bounds.right;
+        var left = _this.pos.x;
+ 
+        if ((_this.vel > 0 && (right + _this.vel) >= me.game.viewport.width) || (_this.vel < 0 && (left + _this.vel) <= 0)) 
+        {
+            _this.vel *= -1;
+            _this.pos.y += 16;
+            if (_this.vel > 0)
+            {
+                _this.vel += 5;
+            }
+            else 
+            {
+                _this.vel -= 5;
+            }
+        }
+        else 
+        {
+            _this.pos.x += _this.vel;
+        }
+        game.playScreen.checkIfLoss(_this.pos.y + _this.childBounds.bottom);
+    }, 1000);
+  },
+  
+ 
+  onDeactivateEvent: function () 
+  {
+      me.timer.clearInterval(this.timer);
+  },
+  
+//this.childBounds = this.getBounds().clone();
+  
+  addChild: function (child, z) 
+  {
+    this._super(me.Container, "addChild", [child, z]);
+    this.resizeChildBounds();
+  },
+  
+  removeChildNow: function (child)
+  {
+    this._super(me.Container, "removeChildNow", [child]);
+    this.resizeChildBounds();
+  },
+  
+  
+  resizeChildBounds: function ()
+  {
+      this.childBounds.pos.set(Infinity, Infinity);
+      this.childBounds.resize(-Infinity, -Infinity);
+      for (var i = this.children.length, child; i--, (child = this.children[i]);) 
+      {
+          if (child.isRenderable)
+          {
+              this.childBounds.union(child);
+          }
+      }
+  },
+  
+  
+  update: function (time) 
+  {
+    if (this.children.length === 0 && this.createdEnemies) 
+    {
+      game.playScreen.reset();
+    }
+    this._super(me.Container, "update", [time]);
+  }
+  
+  
+  
+});
--- js/game.js
+++ js/game.js
@@ -11,41 +11,46 @@ var game = {
 
     // Run on page load.
     "onload" : function () {
-    // Initialize the video.
-    if (!me.video.init("screen",  me.video.CANVAS, 960, 640, true, 'auto')) {
-        alert("Your browser does not support HTML5 canvas.");
-        return;
-    }
-
-    // add "#debug" to the URL to enable the debug Panel
-    if (document.location.hash === "#debug") {
-        window.onReady(function () {
-            me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
-        });
-    }
-
-    // Initialize the audio.
-    me.audio.init("mp3,ogg");
-
-    // Set a callback to run when loading is complete.
-    me.loader.onload = this.loaded.bind(this);
-
-    // Load the resources.
-    me.loader.preload(game.resources);
-
-    // Initialize melonJS and display a loading screen.
-    me.state.change(me.state.LOADING);
-},
-
+        // Initialize the video.
+        if (!me.video.init(640, 480, {wrapper : "screen", scale : 'auto'})) {
+            alert("Your browser does not support HTML5 canvas.");
+            return;
+        }
+ 
+        // add "#debug" to the URL to enable the debug Panel
+        if (document.location.hash === "#debug") {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
+        }
+ 
+        // Initialize the audio.
+        me.audio.init("mp3,ogg");
+ 
+        // Set a callback to run when loading is complete.
+        me.loader.onload = this.loaded.bind(this);
+ 
+        // Load the resources.
+        me.loader.preload(game.resources);
+ 
+        // Initialize melonJS and display a loading screen.
+        me.state.change(me.state.LOADING);
+    },
+ 
+     
+ 
     // Run on game resources loaded.
     "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
-
-        // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
-
-        // Start the game.
+      
+        me.pool.register("player", game.Player);
+        me.pool.register("enemy", game.Enemy);
+        me.pool.register("laser", game.Laser);
+      
+        // set the "Play/Ingame" Screen Object
+        this.playScreen = new game.PlayScreen();
+        me.state.set(me.state.PLAY, this.playScreen);
+ 
+        // start the game
         me.state.change(me.state.PLAY);
     }
 };
