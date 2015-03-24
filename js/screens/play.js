bea16e477d078c1c25829f05663029a6f08f9da2
wellesleywatson <git@github.com:wellesleywatson>
2015-03-24 00:28:49 +0000

    space invaders working with one error

--- js/screens/play.js
+++ js/screens/play.js
@@ -1,45 +1,85 @@
 game.PlayScreen = me.ScreenObject.extend({
+    checkIfLoss: function (y) {
+        if (y >= this.player.pos.y) {
+            this.reset();
+        }
+    },
     /**
      *  action to perform on state change
      */
-   checkIfLoss: function (y) 
-    {
-      if (y >= this.player.pos.y) 
-      {
-          this.reset();
-      }
-    },
-  
-  
-    onResetEvent: function() 
-    {
+    onResetEvent: function() {
         me.game.world.addChild(new me.ColorLayer("background", "#000000", 0));
-        me.game.world.addChild(new game.Player(), 1);
-      
-        this.enemyManager = new game.EnemyManager();
-        this.enemyManager.createEnemies();
-        me.game.world.addChild(this.enemyManager, 2);
- 
+        this.player = me.pool.pull("player");
+        me.game.world.addChild(this.player, 1);
+
         me.input.bindKey(me.input.KEY.LEFT, "left");
         me.input.bindKey(me.input.KEY.RIGHT, "right");
         me.input.bindKey(me.input.KEY.A, "left");
         me.input.bindKey(me.input.KEY.D, "right");
         me.input.bindKey(me.input.KEY.SPACE, "shoot", true);
+
+        this.enemyManager = new game.EnemyManager();
+        this.enemyManager.createEnemies();
+        me.game.world.addChild(this.enemyManager, 2);
     },
+
 
     /**
      *  action to perform when leaving this screen (state change)
      */
-     onDestroyEvent: function() 
-    {
+    onDestroyEvent: function() {
         me.input.unbindKey(me.input.KEY.LEFT);
         me.input.unbindKey(me.input.KEY.RIGHT);
         me.input.unbindKey(me.input.KEY.A);
         me.input.unbindKey(me.input.KEY.D);
         me.input.unbindKey(me.input.KEY.SPACE);
     }
-  
-  
-  
-  
 });
+
+
+
+// game.PlayScreen = me.ScreenObject.extend({
+//     /**
+//      *  action to perform on state change
+//      */
+//    checkIfLoss: function (y) 
+//     {
+//       if (y >= this.player.pos.y) 
+//       {
+//           this.reset();
+//       }
+//     },
+  
+  
+//     onResetEvent: function() 
+//     {
+//         me.game.world.addChild(new me.ColorLayer("background", "#000000", 0));
+//         me.game.world.addChild(new game.Player(), 1);
+      
+//         this.enemyManager = new game.EnemyManager();
+//         this.enemyManager.createEnemies();
+//         me.game.world.addChild(this.enemyManager, 2);
+ 
+//         me.input.bindKey(me.input.KEY.LEFT, "left");
+//         me.input.bindKey(me.input.KEY.RIGHT, "right");
+//         me.input.bindKey(me.input.KEY.A, "left");
+//         me.input.bindKey(me.input.KEY.D, "right");
+//         me.input.bindKey(me.input.KEY.SPACE, "shoot", true);
+//     },
+
+//     /**
+//      *  action to perform when leaving this screen (state change)
+//      */
+//      onDestroyEvent: function() 
+//     {
+//         me.input.unbindKey(me.input.KEY.LEFT);
+//         me.input.unbindKey(me.input.KEY.RIGHT);
+//         me.input.unbindKey(me.input.KEY.A);
+//         me.input.unbindKey(me.input.KEY.D);
+//         me.input.unbindKey(me.input.KEY.SPACE);
+//     }
+  
+  
+  
+  
+// });
